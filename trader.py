2c32caa193c40f330ae12863e0e95867eae38e5c
Zia Rehman <iamziarehman@gmail.com>
2024-07-04 01:35:46 +0100

    Added an Orders class to keep track of orders

--- trader.py
+++ trader.py
@@ -10,33 +10,54 @@ from datamodel import Order, OrderDepth, Symbol, TradingState
 
 logger = Logger()
 
+class Orders():
+    def __init__(self):
+        self._orders = {}
+    
+    def get_orders(self):
+        return self._orders 
+    
+    # def __getitem__(self, symbol):
+    #     return self._orders.setdefault(symbol, [])
+    
+    def place_order(self, symbol, price, quantity):
+        BUY_SELL = None
+        if quantity> 0:
+            BUY_SELL = "BUY"
+        elif quantity< 0:
+            BUY_SELL = "SELL"
+        
+        print(f"{BUY_SELL} {quantity} at {price}")
+        self._orders.setdefault(symbol, []).append(Order(symbol, price, quantity))  
+
 class ITrader(ABC):
     def run(self, state: TradingState) -> dict[Symbol, List[Order]]:
         raise NotImplementedError()
 
-def strategy_1(state: TradingState, product: Symbol, acceptable_bid_price: int, acceptable_ask_price: int) -> list[Order]:
+
+def strategy_1(state: TradingState, orders: Orders, product: Symbol, acceptable_bid_price: int, acceptable_ask_price: int):
     """
     Buys and sell product based on an acceptable price.
     """
-    order_depth: OrderDepth = state.order_depths[product]
-    orders: List[Order] = []
+    order_depth = state.order_depths.get(product)
+
+    if order_depth is None:
+        return
+
     print("Acceptable price : " + str(acceptable_ask_price))
     print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
     if len(order_depth.sell_orders) != 0:
         best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
         if int(best_ask) < acceptable_ask_price:
-            print("BUY", str(-best_ask_amount) + "x", best_ask)
-            orders.append(Order(product, best_ask, -best_ask_amount))
+            orders.place_order(product, best_ask, -best_ask_amount)
     
     if len(order_depth.buy_orders) != 0:
         best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
         if int(best_bid) > acceptable_bid_price:
             print("SELL", str(best_bid_amount) + "x", best_bid)
-            orders.append(Order(product, best_bid, -best_bid_amount))
+            orders.place_order(product, best_bid, -best_bid_amount)
     
-    return orders
-
 def ema(price_history: list[float], span: int) -> int:
     """
     Method to calculate exponential moving average.
@@ -46,19 +67,19 @@ def ema(price_history: list[float], span: int) -> int:
     data_series = pd.Series(price_history[-span:])
     return int(data_series.ewm(span=span, adjust=False).mean().tail(1))
 
-def round_1(state: TradingState, traderData: dict, orders_to_make: dict):
+def round_1(state: TradingState, traderData: dict, orders: Orders):
     # amethysts 
     acceptable_bid_price_amethysts = 10_000
     acceptable_ask_price_amethysts = 10_000
 
-    orders_to_make[AMETHYSTS] = strategy_1(state=state, product=AMETHYSTS, acceptable_ask_price=acceptable_ask_price_amethysts, acceptable_bid_price=acceptable_bid_price_amethysts)
+    strategy_1(state=state, orders=orders, product=AMETHYSTS, acceptable_ask_price=acceptable_ask_price_amethysts, acceptable_bid_price=acceptable_bid_price_amethysts)
 
     # star fruit
     starfruit_span = 6
     if traderData.get("STARTFRUIT_PRICES"):
         price_of_startfruit = ema(traderData["STARTFRUIT_PRICES"], starfruit_span)
 
-        orders_to_make[STARFRUIT] = strategy_1(state=state, product=STARFRUIT, acceptable_ask_price=price_of_startfruit, acceptable_bid_price=price_of_startfruit)
+        strategy_1(state=state, orders=orders, product=STARFRUIT, acceptable_ask_price=price_of_startfruit, acceptable_bid_price=price_of_startfruit)
 
         
     starfruit_best_ask = min(state.order_depths[STARFRUIT].sell_orders)
@@ -73,20 +94,18 @@ def round_1(state: TradingState, traderData: dict, orders_to_make: dict):
 
 class Trader(ITrader):
     def run(self, state: TradingState):
-        traderData: dict = jsonpickle.decode(state.traderData) if state.traderData else {}
-        orders_to_make = {}
+        traderData = jsonpickle.decode(state.traderData) if state.traderData else {}
+        orders = Orders()
 
-        round_1(state=state, traderData=traderData, orders_to_make=orders_to_make)
+        round_1(state=state, traderData=traderData, orders=orders)
         
         # traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         traderData = jsonpickle.encode(traderData)
         conversions = 1
 
-        logger.flush(state, orders_to_make, conversions, traderData)
-        return orders_to_make, conversions, traderData
         traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         
         conversions = 1
 
-        logger.flush(state, orders_to_make, conversions, traderData)
-        return orders_to_make, conversions, traderData
+        logger.flush(state, orders.get_orders(), conversions, traderData)
+        return orders.get_orders(), conversions, traderData
