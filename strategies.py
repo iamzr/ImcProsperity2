70f85fb740786d9f77ae68d4b7bfa9d5b7c910e0
Zia Rehman <iamziarehman@gmail.com>
2024-07-27 19:21:54 +0100

    Initial code for round 4

--- strategies.py
+++ strategies.py
@@ -3,6 +3,8 @@ import pandas as pd
 from datamodel import Order, OrderDepth, Symbol, TradingState
 from orders import Orders
 from products import CHOCOLATE, GIFT_BASKET, ROSES, STRAWBERRIES
+from products import CHOCOLATE, ROSES, STRAWBERRIES
+from math import exp, log, pi
 
 def ema(price_history: list[float], span: int) -> int:
     """
@@ -146,3 +148,48 @@ def round_3_arbitrage(state, orders: Orders, combined_product, component_product
     #     orders_to_make.setdefault(product, []).append(Order(product, best_asks[combined_product][0], -best_asks[combined_product][1]))
 
 
+
+def norm_cdf(x):
+    """
+    An approximation to the cumulative distribution
+    function for the standard normal distribution:
+    N(x) = \frac{1}{sqrt(2*\pi)} \int^x_{-\infty} e^{-\frac{1}{2}s^2} ds
+    """
+    k = 1.0/(1.0+0.2316419*x)
+    k_sum = k * (0.319381530 + k * (-0.356563782 + \
+        k * (1.781477937 + k * (-1.821255978 + 1.330274429 * k))))
+
+    if x >= 0.0:
+        return (1.0 - (1.0 / ((2 * pi)**0.5)) * exp(-0.5 * x * x) * k_sum)
+    else:
+        return 1.0 - norm_cdf(-x)
+
+def norm_pdf(x):
+    """
+    Standard normal probability density function
+    """
+    return (1.0/((2*pi)**0.5))*exp(-0.5*x*x)
+
+def d_j(j, S, K, r, v, T):
+    """
+    d_j = \frac{log(\frac{S}{K})+(r+(-1)^{j-1} \frac{1}{2}v^2)T}{v sqrt(T)}
+    """
+    return (log(S/K) + (r + ((-1)**(j-1))*0.5*v*v)*T)/(v*(T**0.5))
+
+def vanilla_call_price(S, K, r, v, T):
+    """
+    Price of a European call option struck at K, with
+    spot S, constant rate r, constant vol v (over the
+    life of the option) and time to maturity T
+    """
+    return S * norm_cdf(d_j(1, S, K, r, v, T)) - \
+        K*exp(-r*T) * norm_cdf(d_j(2, S, K, r, v, T))
+
+def vanilla_put_price(S, K, r, v, T):
+    """
+    Price of a European put option struck at K, with
+    spot S, constant rate r, constant vol v (over the
+    life of the option) and time to maturity T
+    """
+    return -S * norm_cdf(-d_j(1, S, K, r, v, T)) + \
+        K*exp(-r*T) * norm_cdf(-d_j(2, S, K, r, v, T))
