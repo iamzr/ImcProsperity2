f926e2ac004decf96af0091c3a70ccd0b3b67957
Zia Rehman <iamziarehman@gmail.com>
2024-07-28 01:12:30 +0100

    Refactored rounds

--- rounds.py
+++ rounds.py
@@ -1,97 +1,112 @@
+from abc import ABC, abstractmethod
 from datamodel import TradingState
 from orders import Orders
 from products import AMETHYSTS, CHOCOLATE, COCONUT, COCONUT_COUPON, GIFT_BASKET, ROSES, STARFRUIT, STRAWBERRIES
 from strategies import AcceptablePriceStrategy, AcceptablePriceWithEmaStrategy, SpreadTradingStrategy, VanillaOptionsPricing, ema, get_mid_price 
 
+class Round(ABC):
+    def __init__(self, state: TradingState, traderData: dict, orders: Orders):
+        self._state = state
+        self._trader_data = traderData
+        self._orders = orders
+    
+    @abstractmethod
+    def _run(self):
+        raise NotImplementedError()
+    
+    def run(self):
+        return self._run()
 
-def round_1(state: TradingState, traderData: dict, orders: Orders):
-    # amethysts 
-    if not state.order_depths.get(AMETHYSTS):
-        return
+class Round1(Round):
+    def _run(self):
+        # amethysts 
+        if not self._state.order_depths.get(AMETHYSTS):
+            return
 
-    acceptable_bid_price_amethysts = 10_000
-    acceptable_ask_price_amethysts = 10_000
+        acceptable_bid_price_amethysts = 10_000
+        acceptable_ask_price_amethysts = 10_000
 
-    s = AcceptablePriceStrategy(state=state, orders=orders, product=AMETHYSTS, acceptable_ask_price=acceptable_ask_price_amethysts, acceptable_bid_price=acceptable_bid_price_amethysts)
-    s.run()
+        s = AcceptablePriceStrategy(state=self._state, orders=self._orders, product=AMETHYSTS, acceptable_ask_price=acceptable_ask_price_amethysts, acceptable_bid_price=acceptable_bid_price_amethysts)
+        s.run()
 
-    # star fruit
+        # star fruit
 
-    starfruit_span = 6
+        starfruit_span = 6
 
-    s = AcceptablePriceWithEmaStrategy(state=state, orders=orders, trader_data=traderData, product=STARFRUIT, span=starfruit_span)
-    s.run()
+        s = AcceptablePriceWithEmaStrategy(state=self._state, orders=self._orders, trader_data=self._trader_data, product=STARFRUIT, span=starfruit_span)
+        s.run()
 
 def get_gift_basket_price(chocolate, roses, strawberries):
     return 4 * chocolate + roses + 6 * strawberries
 
-def round_3(state: TradingState, traderData: dict, orders: Orders):
-    component_products = [CHOCOLATE, STRAWBERRIES, ROSES]
-    combined_product = [GIFT_BASKET]
+class Round3(Round):
+    def _run(self):
+        component_products = [CHOCOLATE, STRAWBERRIES, ROSES]
+        combined_product = [GIFT_BASKET]
 
-    gift_basket_price = get_mid_price(state, GIFT_BASKET)
-    
-    if gift_basket_price is None:
-        return
+        gift_basket_price = get_mid_price(self._state, GIFT_BASKET)
 
-    components_price = get_gift_basket_price(get_mid_price(state, CHOCOLATE), get_mid_price(state, ROSES), get_mid_price(state, STRAWBERRIES))
+        if gift_basket_price is None:
+            return
 
-    SPREAD_MEAN = 376.0862
-    SPREAD_STD = 76.354
+        components_price = get_gift_basket_price(get_mid_price(self._state, CHOCOLATE), get_mid_price(self._state, ROSES), get_mid_price(self._state, STRAWBERRIES))
 
-    s = SpreadTradingStrategy(state=state, 
-                              orders=orders, 
-                              portfolio_1=combined_product, 
-                              portfolio_1_price=int(gift_basket_price),
-                              portfolio_2=component_products,
-                              portfolio_2_price=int(components_price),
-                              spread_mean=SPREAD_MEAN,
-                              spread_std=SPREAD_STD,
-                              threshold=1
-                              )
-    s.run()
+        SPREAD_MEAN = 376.0862
+        SPREAD_STD = 76.354
 
+        s = SpreadTradingStrategy(state=self._state, 
+                                  orders=self._orders, 
+                                  portfolio_1=combined_product, 
+                                  portfolio_1_price=int(gift_basket_price),
+                                  portfolio_2=component_products,
+                                  portfolio_2_price=int(components_price),
+                                  spread_mean=SPREAD_MEAN,
+                                  spread_std=SPREAD_STD,
+                                  threshold=1
+                                  )
+        s.run()
 
-def round_4(state: TradingState, traderData: dict, orders: Orders):
+class Round4(Round):
+    def _run(self):
 
-    coconuts_midprice = get_mid_price(state, COCONUT) # price of coconuts
+        coconuts_midprice = get_mid_price(self._state, COCONUT) # price of coconuts
 
-    if coconuts_midprice is None:
-        return
+        if coconuts_midprice is None:
+            return
 
-    coupons_midprice = get_mid_price(state, COCONUT_COUPON) 
+        coupons_midprice = get_mid_price(self._state, COCONUT_COUPON) 
 
-    if coupons_midprice is None:
-        return
+        if coupons_midprice is None:
+            return
 
-    # for coconut coupons
-    strike_price = 10_000
-    time_to_maturity = 250/365
+        # for coconut coupons
+        strike_price = 10_000
+        time_to_maturity = 250/365
 
-    risk_free_rate = 0.0
-    vol = 0.19 # volativity of the coconuts
+        risk_free_rate = 0.0
+        vol = 0.19 # volativity of the coconuts
 
-    price_of_coupons = VanillaOptionsPricing().call_price(coconuts_midprice, strike_price, risk_free_rate, vol, time_to_maturity)
-    # price_of_coupons = 600
+        price_of_coupons = VanillaOptionsPricing().call_price(coconuts_midprice, strike_price, risk_free_rate, vol, time_to_maturity)
+        # price_of_coupons = 600
 
-    print("here", coupons_midprice, price_of_coupons)
+        print("here", coupons_midprice, price_of_coupons)
 
-    s = AcceptablePriceStrategy(state=state, orders=orders, product=COCONUT_COUPON, acceptable_ask_price=price_of_coupons, acceptable_bid_price=price_of_coupons)
-    s.run()
+        s = AcceptablePriceStrategy(state=self._state, orders=self._orders, product=COCONUT_COUPON, acceptable_ask_price=price_of_coupons, acceptable_bid_price=price_of_coupons)
+        s.run()
 
-    # part 2
-    SPREAD_MEAN = 9000.263400
-    SPREAD_STD = 50.559215
-    # SPREAD_MEAN = 9300 SPREAD_STD = 2k
+        # part 2
+        SPREAD_MEAN = 9000.263400
+        SPREAD_STD = 50.559215
+        # SPREAD_MEAN = 9300 SPREAD_STD = 2k
 
-    s = SpreadTradingStrategy(state=state, 
-                              orders=orders, 
-                              portfolio_1=[COCONUT], 
-                              portfolio_1_price=int(coconuts_midprice),
-                              portfolio_2=[COCONUT_COUPON],
-                              portfolio_2_price=int(coupons_midprice),
-                              spread_mean=SPREAD_MEAN,
-                              spread_std=SPREAD_STD,
-                              threshold=2
-                              )
-    # s.run()
+        s = SpreadTradingStrategy(state=self._state, 
+                                  orders=self._orders, 
+                                  portfolio_1=[COCONUT], 
+                                  portfolio_1_price=int(coconuts_midprice),
+                                  portfolio_2=[COCONUT_COUPON],
+                                  portfolio_2_price=int(coupons_midprice),
+                                  spread_mean=SPREAD_MEAN,
+                                  spread_std=SPREAD_STD,
+                                  threshold=2
+                                  )
+        # s.run()
--- trader.py
+++ trader.py
@@ -7,28 +7,23 @@ import pandas as pd
 from orders import Orders
 from logger import Logger
 from datamodel import Order, Symbol, TradingState
-from rounds import round_1, round_3, round_4
+from rounds import Round1, Round3, Round4
 
 logger = Logger()
 
-class ITrader(ABC):
-    def run(self, state: TradingState) -> dict[Symbol, List[Order]]:
-        raise NotImplementedError()
-
-
-class Trader(ITrader):
+class Trader:
     def run(self, state: TradingState):
         traderData = jsonpickle.decode(state.traderData) if state.traderData else {}
         orders = Orders()
 
-        round_1(state=state, traderData=traderData, orders=orders)
-        round_3(state=state, traderData=traderData, orders=orders)
-        round_4(state=state, traderData=traderData, orders=orders)
+        Round1(state=state, traderData=traderData, orders=orders).run() # type: ignore
+        Round3(state=state, traderData=traderData, orders=orders).run() # type: ignore
+        Round4(state=state, traderData=traderData, orders=orders).run() # type: ignore
         
         # traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         traderData = jsonpickle.encode(traderData)
         conversions = 1
 
-        logger.flush(state, orders.get_orders(), conversions, traderData)
+        logger.flush(state, orders.get_orders(), conversions, traderData) # type: ignore
         return orders.get_orders(), conversions, traderData
     
